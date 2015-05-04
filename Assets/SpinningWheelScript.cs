ea5c94bdd9787881ac185f9ab28172c3baaec92c
Henning Ax <henning-ax@web.de>
2015-05-04 19:27:10 +0200

    Comment code of SpinningWheelScript

--- Assets/SpinningWheelScript.cs
+++ Assets/SpinningWheelScript.cs
@@ -3,13 +3,10 @@ using System.Collections;
 
 public class SpinningWheelScript : MonoBehaviour {
 
-    //Predifined Rotations
-    public GameObject LeftRot;
-    public GameObject RightRot;
-    public GameObject UpRot;
-    public GameObject DownRot;
+    //Inspector variables
     public float FrotSpeedSource;
 
+    //Local variables
     Quaternion TargetRot;
     Quaternion StartRot;
     float FstartTime;
@@ -36,6 +33,7 @@ public class SpinningWheelScript : MonoBehaviour {
             float FdistCovered = (Time.time - FstartTime) * FrotSpeed;
             if (FrotAngle != 0)
             {
+                //Rotate the wheel with ease in and ease out
                 float FrotFrac = FdistCovered / FrotAngle;
                 //float FsmoothDistance = Mathf.SmoothStep(0, 1, FrotFrac);
                 float FsmoothDistance = smootherstep(0, 1, FrotFrac);
@@ -54,35 +52,13 @@ public class SpinningWheelScript : MonoBehaviour {
 
     void GravityChange()
     {
-
-        //if(transform.parent.up.normalized == Physics.gravity.normalized)
-        //{
-        //    TargetRot = UpRot.transform.rotation;
-        //}
-        //else
-        //{
-        //    if(-transform.parent.up.normalized == Physics.gravity.normalized)
-        //    {
-        //        TargetRot = DownRot.transform.rotation;
-        //    } 
-        //    else
-        //    {
-        //        if(transform.parent.forward.normalized == Physics.gravity.normalized)
-        //        {
-        //            TargetRot = LeftRot.transform.rotation;
-        //        }
-        //        else
-        //        {
-        //            if(-transform.parent.forward.normalized == Physics.gravity.normalized)
-        //            {
-        //                TargetRot = RightRot.transform.rotation;
-        //            }
-        //        }
-        //    }
-        //}
+        //Project the gravity vector on the plane the wheel is onto
         Vector3 GravityProjected = Vector3.ProjectOnPlane(Physics.gravity, transform.forward);
+        //if the magnitude of this vector is zero the wheel will not spin because the gravity parallel to the axis of the wheel
         if (GravityProjected.magnitude != 0)
         {
+            //Get the new rotation of the wheel by making the up vector of the wheel, which is facing the heayiest point of the wheel, the same as the projected gravity vector
+            //Because the gravity vector is projected to the plan of the wheel, the wheel can only spin around its forward axis, which is parralel to its physic axis
             TargetRot = Quaternion.LookRotation(transform.forward, GravityProjected);
             StartRot = transform.rotation;
             FrotSpeed = FrotSpeedSource * GravityProjected.magnitude;
@@ -93,6 +69,7 @@ public class SpinningWheelScript : MonoBehaviour {
 
     }
 
+    //Very smooth interpolate between f1 und f2
     float smootherstep(float edge0, float edge1, float x)
     {
         // Scale, and clamp x to 0..1 range
