e7cc86c8332c162e81306942030271eb2dbb3800
Philipp Liese <philipp.liese@stud.h-da.de>
2015-05-18 14:34:04 +0200

    imported catwalk and grid floor

---
+++ Assets/Scripts/ActivateByPickupCrash.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateByPickupCrash : MonoBehaviour {
+
+    public float FactivateThreshold = 10.0f;
+    Rigidbody RigidbodyComp;
+
+	// Use this for initialization
+	void Awake () {
+       RigidbodyComp = this.gameObject.GetComponent<Rigidbody>();
+       RigidbodyComp.isKinematic = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.collider.tag == "PickUp" && col.relativeVelocity.magnitude > FactivateThreshold && RigidbodyComp.isKinematic && col.collider.GetComponent<Rigidbody>().useGravity)
+        {
+            RigidbodyComp.isKinematic = false;
+            RigidbodyComp.velocity = col.relativeVelocity;
+        }
+    }
+}
