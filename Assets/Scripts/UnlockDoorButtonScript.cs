ecce46bb1076a2427902f41e8ca1c491d1121af1
Henning Ax <henning-ax@web.de>
2015-06-18 16:44:13 +0200

    Added Materials to factory room

---
+++ Assets/Scripts/UnlockDoorButtonScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnlockDoorButtonScript : ButtonTarget {
+
+    Rigidbody rbComp;
+
+	// Use this for initialization
+	void Start () {
+        rbComp = this.GetComponent<Rigidbody>();
+        rbComp.isKinematic = true;
+	
+	}
+
+    public override void TargetActivate()
+    {
+        base.TargetActivate();
+        rbComp.isKinematic = false;
+        
+    }
+	
+}
